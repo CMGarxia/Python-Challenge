b7ceef694a05c6562ade9f9020ce9ca1759ead73
CMGarxia <152096353+CMGarxia@users.noreply.github.com>
2024-01-27 16:42:56 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,84 +1,83 @@
 #dependencies
-import os 
+import os
 import csv
 
 #path to csv file
-budget_data_csv = os.path.join("Resources", "budget_data.csv")
+budget_data_csv = os.path.join("Resources","budget_data.csv")
 
 #csv_path = "/Users/rebelplanet/Desktop/Python-Challenge /PyBank/Resources/budget_data.csv"
 #def financial_analysis(csv_path):
 
 #list variables
+total_months = 0
+total_profit_losses = 0
 previous_profit_losses = None
 changes = []
-total_profit_losses = 0 
-total_months = 0 
 greatest_increase = 0
 greatest_increase_date = ""
 greatest_decrease = 0
 greatest_decrease_date = ""
 
-    #read csv file in read mode
-with open(budget_data_csv, 'r') as budget_csv:
-        csv_reader = csv.reader(budget_csv)
-        row_header = next(csv_reader, None) #store header
-        # loop rows in csv
-        for row in csv_reader:
-            total_months += 1
-            #print(f"Total Months: {total_months}")
+#read csv file in read mode
+with open(budget_data_csv, 'r') as budget_csv:      
+    csv_reader = csv.reader(budget_csv)
+    row_header = next(csv_reader, None)              #store header 
 
-with open(budget_data_csv, 'r') as budget_csv:    #read csv file in read mode
-     csv_reader = csv.reader(budget_csv)
-     next(csv_reader, None)       #store header
-            
+    for row in csv_reader:
+        total_months += 1
 
-     for row in csv_reader:
-            profit_losses = int(row[1])
-            total_profit_losses += profit_losses
+#print(f"Total Months: {total_months}")
+
+with open(budget_data_csv, 'r') as budget_csv:     #reas csv file in read mode 
+    csv_reader = csv.reader(budget_csv)
+    next(csv_reader, None)              #store header
+
+    for row in csv_reader:
+        profit_losses = int(row[1])
+        total_profit_losses += profit_losses
 
 formatted_total = "${:.0f}".format(total_profit_losses)
 
 #read csv file using module and create csv_reader obj.
-with open(budget_data_csv, 'r') as budget_csv:     #open in readmode
-      csv_reader = csv.reader(budget_csv)
-      row_header = next(csv_reader, None)   #store header
+with open(budget_data_csv, 'r') as budget_csv:     #open in readmode 
+    csv_reader = csv.reader(budget_csv)
+    row_header = next(csv_reader, None)      #store header 
 
-      for row in csv_reader:
-          current_profit_losses = int(row[1])
-          if previous_profit_losses is not None:
-                change = current_profit_losses - previous_profit_losses
-                changes.append(change)
-                previous_profit_losses = current_profit_losses
-            
+    for row in csv_reader:
+        current_profit_losses = int(row[1])
+        if previous_profit_losses is not None:
+            change = current_profit_losses - previous_profit_losses
+            changes.append(change)
+        previous_profit_losses = current_profit_losses
+
 average_change = sum(changes)/len(changes)
 formatted_average_change = "${:.2f}".format(average_change)
 
-with open(budget_data_csv, 'r') as budget_csv:  #open in read mode
-     csv_reader = csv.reader(budget_csv)
-     next(csv_reader, None)   #store header
+with open(budget_data_csv, 'r') as budget_csv:     #open in read mode 
+    csv_reader = csv.reader(budget_csv)
+    next(csv_reader, None)    #store header
 
+    for row in csv_reader:
+        current_profit_losses = int(row[1])
+        date = row[0]
+        #Conditional to find Greatest Increase and Decrease in Profits
+        if previous_profit_losses is not None:
+            change = current_profit_losses - previous_profit_losses
 
-     for row in csv_reader:
-           current_profit_losses = int(row[1])
-           date = row[0]
+            if change > greatest_increase:
+                greatest_increase = change
+                greatest_increase_date = date
+            
+            if change < greatest_decrease:
+                greatest_decrease = change
+                greatest_decrease_date = date
 
-           #conditional for Greatest Increase and Decrease in profits
-           if previous_profit_losses is not None:
-                 change = current_profit_losses - previous_profit_losses 
+        previous_profit_losses = current_profit_losses
 
-                 if change > greatest_increase:
-                       greatest_increase = change
-                       greatest_increase_date = date
+#print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")
+        
 
-                 if change < greatest_decrease: 
-                       greatest_decrease = change
-                       greatest_decrease_date = date
-
-           previous_profit_losses = current_profit_losses
-           
-#print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})")  
-
-##print formatted outputs
+##Print Outputs in the required format
 print("Financial Analysis")
 
 print("----------------------------")
@@ -99,8 +98,8 @@ print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_incre
 print("----------------------------")
 
 print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})")
-      
-#output results to text file
+
+#Output results to a text file
 output_file = os.path.join("analysis", "budget_data_results.txt")
 
 with open(output_file, 'w') as txtfile:
@@ -114,5 +113,4 @@ with open(output_file, 'w') as txtfile:
     txtfile.write("----------------------------\n")
     txtfile.write(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase}\n")
     txtfile.write("----------------------------\n")
-    txtfile.write(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease}\n")        
-    
+    txtfile.write(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease}\n")
